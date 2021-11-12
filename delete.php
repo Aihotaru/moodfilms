a330e74e8fc6323c0696b5f6b60248194e0aab68
Aihotaru <biyakko@mail.ru>
2021-11-12 15:01:38 +0300

    Added functional delete button

--- delete.php
+++ delete.php
@@ -1,12 +1,12 @@
 <?php
-$deleteIndex = $_POST['deleteIndex'];
+$delIndex = $_POST['deletion'];
 $con = new mysqli("127.0.0.1", "root", "fuckYOU", "movies", 3305);
 if($con->connect_error){
     echo 'Connection Faild: '.$con->connect_error;
 }else{
-    $sql = mysqli_query($con, "DELETE FROM films WHERE id = '$deleteIndex'");
+    $sql = mysqli_query($con, "DELETE FROM films WHERE id = '$delIndex'");
 
     if ($sql){
-        echo  'fuck you';
+        echo  'Successful';
     }
 }
--- index.php
+++ index.php
@@ -13,7 +13,8 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-beta1/jquery.min.js" type="text/javascript"></script>
+<!--    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-beta1/jquery.min.js" type="text/javascript"></script>-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
 </head>
 <body>
@@ -197,7 +198,7 @@
                                 <p class="genre">' . $row['genre'] . '</p>
                                 <div class="tag ' . $row['tag'] . '" title="' . $row['tag'] . '"> </div>
                             </div>
-                            <button class="del_but" type="button" name="' . $row['name'] . '" index="' . $row['id'] . '">Удалить</button>
+                            <button class="del_but" id="del_action" type="button"  name="' . $row['id'] . '" index="' . $row['id'] . '">Удалить</button>
                        </div>
                     </div>
            ';
@@ -273,22 +274,27 @@
     </script>
     <script>
 
-        $('.del_but').on('click', (function() {
-            var deleteIndex = $(this).attr('index');
-            $.ajax({
-                type: 'POST',
-                url: 'delete.php',
-                data: {'deleteIndex': deleteIndex},
-                processData: false,
-                contentType: false,
-//                success: function(){
-//                    location.reload();
-//                },
-                error: function(){
-                    alert('Fail!');
-                }
+        $(document).ready(function()
+        {
+            $(".del_but").click(function()
+            {
+                var deleteIndex = $(this).attr('index');
+                var datadel = $( "#del_action" ).serialize();
+
+                $.ajax
+                ({
+                    type: "POST",
+                    url: "delete.php",
+                    data: datadel + '&deletion=' + deleteIndex,
+                    cache: false,
+                    success: function(html)
+                    {
+                        location.reload();
+                    }
+                });
+
             });
-        }));
+        });
 
 
     </script>
