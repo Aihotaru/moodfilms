ccb011bce057eec3f5c9751d3ac58b372b2b1031
Aihotaru <biyakko@mail.ru>
2021-11-16 16:05:50 +0300

    Stylish renovations

--- index.php
+++ index.php
@@ -26,28 +26,28 @@
     $result = mysqli_query($mysqli, $sql);
 
     ?>
-    <nav class="navbar navbar-light bg-ligh ">
-        <div class="container-fluid justify-content">
+    <nav class="navbar navbar-light bg-ligh">
+        <div class="container-fluid justify-content ">
             <div>
-                <button class="btn btn-outline-info mr-2 show" type="button" title="Filter" data-toggle="modal" data-target="#filters">
+                <button class="btn btn-outline-info mr-2 show but-nav" type="button" title="Filter" data-toggle="modal" data-target="#filters">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-filter" viewBox="0 0 16 16">
                         <path d="M6 10.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1h-3a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/>
                     </svg>
                 </button>
 
-                <button class="btn btn-outline-info mr-2 " type="submit" name="my_button" title="Random" data-toggle="modal" data-target="#random" id="icon_events_header">
+                <button class="btn btn-outline-info mr-2 but-nav" type="submit" name="my_button" title="Random" data-toggle="modal" data-target="#random" id="icon_events_header">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dice-3" viewBox="0 0 16 16">
                         <path d="M13 1a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h10zM3 0a3 3 0 0 0-3 3v10a3 3 0 0 0 3 3h10a3 3 0 0 0 3-3V3a3 3 0 0 0-3-3H3z"/>
                         <path d="M5.5 4a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm8 8a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm-4-4a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
                     </svg>
                 </button>
 
-                <button class="btn btn-outline-info mr-2 " id="search" type="button" title="Search" value="Click" onmousedown="viewDiv()">
+                <button class="btn btn-outline-info mr-2 but-nav" id="search" type="button" title="Search" value="Click" onmousedown="viewDiv()">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                         <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
                     </svg>
                 </button>
-                <button class="btn btn-outline-info mr-2 " id="" type="button" title="Add" data-toggle="modal" data-target="#adding">
+                <button class="btn btn-outline-info mr-2 but-nav" id="" type="button" title="Add" data-toggle="modal" data-target="#adding">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-square" viewBox="0 0 16 16">
                         <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
                         <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
@@ -55,8 +55,8 @@
                 </button>
                 <div id="div1">
                     <form id="searchForm" role="form">
-                         <input type="text" name="search" id="search_content">
-                         <button class="btn btn-outline-info mr-2 show" type="submit" name="submit" id="search_handler" data-toggle="modal" data-target="#search_result"></button>
+                         <input type="text" name="search" id="search_content" class="form-control">
+                         <button class="btn btn-outline-info mr-2 show but-nav form" type="submit" name="submit" id="search_handler" data-toggle="modal" data-target="#search_result">Find</button>
                     </form>
                 </div>
             </div>
@@ -104,7 +104,7 @@
         <!-- Modal for random -->
         <div class="modal fade" id="random" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog card-font">
-                <div class="modal-content ">
+                <div class="modal-content card-font ">
                     <div class="modal-header">
                         <h5 class="modal-title" id="exampleModalLabel">Random movie</h5>
                         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -121,7 +121,7 @@
     <!-- Modal for search -->
     <div class="modal fade" id="search_result" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
-            <div class="modal-content">
+            <div class="modal-content card-font">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -137,7 +137,7 @@
     <!-- Modal for adding -->
     <div class="modal fade" id="adding" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
-            <div class="modal-content">
+            <div class="modal-content card-font">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel">Adding movie</h5>
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -163,12 +163,8 @@
                         </select>
                         <label for="add_image" class="form-label">Image:</label>
                         <input class="form-control" type="file" id="add_image"  name="add-image">
-                        <button type="submit" class="btn btn-primary">Save changes</button>
+                        <button type="submit" class="btn add_button center">Add movie</button>
                     </form>
-                </div>
-                <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-
                 </div>
             </div>
         </div>
@@ -176,8 +172,8 @@
     <div class="container" >
         <div class="row " id="Container">
             <div class="backNav hidding">
-                <button class="btn btn-outline-info mr-2 hide">
-                    <a href="index.php">
+                <button class="btn btn-outline-info mr-2 hide but-nav">
+                    <a href="index.php" class="but-nav">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-backspace" viewBox="0 0 16 16">
                             <path d="M5.83 5.146a.5.5 0 0 0 0 .708L7.975 8l-2.147 2.146a.5.5 0 0 0 .707.708l2.147-2.147 2.146 2.147a.5.5 0 0 0 .707-.708L9.39 8l2.146-2.146a.5.5 0 0 0-.707-.708L8.683 7.293 6.536 5.146a.5.5 0 0 0-.707 0z"/>
                             <path d="M13.683 1a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-7.08a2 2 0 0 1-1.519-.698L.241 8.65a1 1 0 0 1 0-1.302L5.084 1.7A2 2 0 0 1 6.603 1h7.08zm-7.08 1a1 1 0 0 0-.76.35L1 8l4.844 5.65a1 1 0 0 0 .759.35h7.08a1 1 0 0 0 1-1V3a1 1 0 0 0-1-1h-7.08z"/>
